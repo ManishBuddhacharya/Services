d48f37f4e663958194a561787c73e2724a66a930
Softwarica College Lab <softwarica@softwarica.com>
2019-06-06 08:33:59 +0545

    services

---
+++ app/src/main/java/com/example/services/BroadcastRecieverExample.java
@@ -0,0 +1,26 @@
+package com.example.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class BroadcastRecieverExample extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        boolean noConnectivity;
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+            noConnectivity = intent.getBooleanExtra(
+                    ConnectivityManager.EXTRA_NO_CONNECTIVITY, false
+            );
+            if (noConnectivity){
+                Toast.makeText(context,"Disconnected", Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(context,"Connected", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/services/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.services;
+
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnfirst, btnSecond;
+    NotificationManagerCompat notificationManagerCompat;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnfirst = findViewById(R.id.btnFirst);
+        btnSecond = findViewById(R.id.btnSecond);
+
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+
+        btnfirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification();
+            }
+        });
+
+        btnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification1();
+            }
+        });
+    }
+
+    private void displayNotification1() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"channel1")
+                .setSmallIcon(R.drawable.manish_icon)
+                .setContentTitle("Hello App")
+                .setContentText("Hello !!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+
+        notificationManagerCompat.notify(1, builder.build());
+
+    }
+
+    private void displayNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"channel2")
+                .setSmallIcon(R.drawable.manish_icon)
+                .setContentTitle("Hello App")
+                .setContentText("Hello From the other side")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+
+        notificationManagerCompat.notify(2, builder.build());
+    }
+
+    BroadcastRecieverExample broadcastRecieverExample = new BroadcastRecieverExample();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(broadcastRecieverExample, intentFilter);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastRecieverExample);
+    }
+}
